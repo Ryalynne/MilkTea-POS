f59e649021e6476b512adbe924b8aece47285632
RySel <104247395+Ryalynne@users.noreply.github.com>
2024-06-05 13:15:23 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta http-equiv="Content-Security-Policy"
+    content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.bunny.net">
